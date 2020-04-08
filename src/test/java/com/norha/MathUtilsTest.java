e06a54c92fd8a0a9e942f6fc722a5694f427332c
Robinclear <robin.peter.vkm@gmail.com>
2020-04-08 17:37:21 -0300

    @BeforeEach

--- src/test/java/com/norha/MathUtilsTest.java
+++ src/test/java/com/norha/MathUtilsTest.java
@@ -4,6 +4,10 @@ package com.norha;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
@@ -13,8 +17,6 @@ class MathUtilsTest {
 	 * 
 	 * and informs the junit engin what methods need to run
 	 */
-
-	
 	
 	
 	/*
@@ -41,7 +43,48 @@ class MathUtilsTest {
 	 * assertIterableEquals(expectedArray, actualArray)
 	 */
 		
+	/*
+	 * Test Life Cycle
+	 * 
+	 * Process in which the test instance are created, managed, and distroied
+	 * 
+	 * JUnit manage the lif cycle of test calss instances.
+	 * 
+	 * JUnit Provide As some Hooks to exicute code in certain point of Life cycle
+	 * 
+	 * 1 .suppose there is three methods for test then the method run is random
+	 * order
+	 * 
+	 * 2 . every test method run indipented instance it crates
+	 * 
+	 * 3. common trap is creates the state outside the method which is irrelevent in
+	 * the case of juint5
+	 * 
+	 * 4 Hooks we have in the Junit Life Cycle they are
+	 * 
+	 * 1. Inititlaze before anything in the class 2. Tear down after all method are
+	 * done
+	 * 
+	 * 3. Initialization before each method 4. Tear Down after Each Method.
+	 * 
+	 * 
+	 * @BeforeAll
+	 * 
+	 * @AfterAll
+	 * 
+	 * @BeforeEach
+	 * 
+	 * @AfterEach
+	 * 
+	 * 
+	 */
 		
+	MathUtils mathUtils;
+	
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
 	
 	@Test
 	void testAdd() {
