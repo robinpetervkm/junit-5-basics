702af35f67d03c99dc7667ab3d47a76f6e61bcdc
Robinclear <robin.peter.vkm@gmail.com>
2020-04-08 16:30:37 -0300

    Sub the method
    and wrote the final outcome in the assert statement
    
    test to fail

--- src/main/java/com/norha/MathUtils.java
+++ src/main/java/com/norha/MathUtils.java
@@ -5,9 +5,17 @@ public class MathUtils {
 	public int add(int a,int b) {
 		return a+b;
 	}
+	
+	/*
+	 * step 1 Stub ur method
+	 * 
+	 * step 2 final expected value in the test assert and make the test fail
+	 * 
+	 * then code the subbed method and make test to pass
+	 */
 
 	public double computeCircleArea(double radius) {
-		return Math.PI*radius*radius;
+		return 0;
 	}
 	
 }
